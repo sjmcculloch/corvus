110b62f6171259898ef7bcf37c19611e56cb3a02
Scott McCulloch <smcculloch@gmail.com>
2018-01-03 15:37:20 +1100

    first

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
 export default () => (
   <View style={styles.container}>
     <Text>Open up App.js to start working on your app!</Text>
-            <Text>Changes you make will automatically reload.</Text>
+    <Text>Changes you make will automatically reload.</Text>
     <Text>Shake your phone to open the developer menu.</Text>
   </View>
 );
